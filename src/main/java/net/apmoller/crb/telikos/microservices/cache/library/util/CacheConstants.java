d28a50c0669581a366a282e97e027babee836299
srikanth-srinivasa <84323252+srikanth-srinivasa@users.noreply.github.com>
2022-12-22 15:56:51 +0530

    Create CacheConstants.java

---
+++ src/main/java/net/apmoller/crb/telikos/microservices/cache/library/util/CacheConstants.java
@@ -0,0 +1,15 @@
+package net.apmoller.crb.telikos.microservices.cache.library.util;
+
+public interface CacheConstants {
+
+    String REDIS_CACHE_TTL = "redis.cache-ttl";
+
+    String REDIS_SSL = "redis.ssl";
+    String REDIS_SSL_REDIS = "redis://";
+    String REDIS_SSL_REDISS = "rediss://";
+
+    String REDIS_HOST = "redis.host";
+    String REDIS_PORT = "redis.port";
+
+
+}
